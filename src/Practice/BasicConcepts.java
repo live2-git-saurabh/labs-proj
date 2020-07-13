7d99aef565f7bfbceff6bd487f44753f98b4f8b5
live2-git-saurabh <saurabh.vnit1107@gmail.com>
2020-07-13 19:21:27 -0400

    new file added

---
+++ src/Practice/BasicConcepts.java
@@ -0,0 +1,19 @@
+package Practice;
+
+public class BasicConcepts {
+	
+	public static void main(String[] args) {
+		
+		supportingClass myMethod = new supportingClass();
+		int myNumber = supportingClass.myMethod(6);
+		
+		int totalDaysInJune = myNumber * 30;
+		System.out.println(totalDaysInJune);
+		
+	}
+	
+
+
+}
+
+
